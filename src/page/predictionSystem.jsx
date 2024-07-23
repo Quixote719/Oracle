6ce3c2c3fd475df7c73ed90e57985537cc6cdb41
Liang Wang <lwang41@jaguarlandrover.com>
2024-07-23 11:03:11 +0800

    update

--- src/page/predictionSystem.jsx
+++ src/page/predictionSystem.jsx
@@ -1,21 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Modal, Skeleton, Table } from 'antd';
+import { Button, Modal, Skeleton, Alert, Table, DatePicker } from 'antd';
+import dayjs from 'dayjs';
 import AI from '@/asset/image/AI.svg';
+import trainingResult from '@/asset/image/trainingResult.png';
 import car from '@/asset/image/rangeRover.png';
+import carsInLine from '@/asset/image/carsInLine.png';
+import dataLake from '@/asset/image/dataLake.png';
 import store from '@/asset/image/4S.jpg';
 import iphone from '@/asset/image/iphone.svg';
-import { ArrowRightOutlined, ArrowUpOutlined, SwapOutlined } from '@ant-design/icons';
+import { ArrowRightOutlined, SwapOutlined } from '@ant-design/icons';
 import { vehicleDataCol, vehicleData } from '@/constant/VCPS.js';
 import styles from './index.module.less';
+
+const dateFormat = 'YYYY-MM-DD';
 
 const PredictionSystem = () => {
   const [isVcdpModalOpen, setIsVcdpModalOpen] = useState(false);
   const [isClientModalOpen, setIsClientModalOpen] = useState(false);
   const [isVehicleModalOpen, setIsVehicleModalOpen] = useState(false);
+  const [isMLModalOpen, setIsMLModalOpen] = useState(false);
   const [vehicleUploadData, setVehicleUploadData] = useState(null);
   const [alarmInfo, setAlarmInfo] = useState(null);
-
-  const vehicleDColforShown = vehicleDataCol.filter(item => item.dataIndex !== 'operation');
+  const [scheduleRepair, setScheduleRepair] = useState(false);
 
   useEffect(() => {
     let target = vehicleDataCol.find(item => item.dataIndex === 'operation');
@@ -48,6 +54,12 @@ const PredictionSystem = () => {
     }
   };
 
+  const openMLModel = () => {
+    if (!isMLModalOpen) {
+      setIsMLModalOpen(true);
+    }
+  };
+
   const openClientModel = () => {
     if (!isClientModalOpen) {
       setIsClientModalOpen(true);
@@ -58,6 +70,13 @@ const PredictionSystem = () => {
     if (!isVehicleModalOpen) {
       setIsVehicleModalOpen(true);
     }
+  };
+
+  const confirmClientModal = () => {
+    setScheduleRepair(true);
+    setVehicleUploadData(null);
+    setAlarmInfo(null);
+    setIsClientModalOpen(false);
   };
 
   const closeClientModal = () => {
@@ -73,8 +92,6 @@ const PredictionSystem = () => {
       return true;
     }
   };
-
-  console.log(vehicleUploadData, vehicleUploadData?.engine_condition);
 
   return (
     <div className={styles.pageBg}>
@@ -97,7 +114,9 @@ const PredictionSystem = () => {
       <div className={styles.middleSection}>
         <div className={styles.VDPSMod}>
           <div id={styles.vdpsLabel}>VDPS</div>
-          <div id={styles.model}>Machine Learning</div>
+          <div id={styles.model} onClick={openMLModel}>
+            Machine Learning
+          </div>
           <div style={{ height: 60 }}>
             <SwapOutlined
               id={styles.arrowSwapSvg}
@@ -107,12 +126,17 @@ const PredictionSystem = () => {
           <img id={styles.predictionModule} src={AI} alt="AI" />
         </div>
         <div style={{ height: 60 }}>
-          <ArrowUpOutlined
-            id={styles.arrowDownSvg}
-            className={getArrowStyle(2) ? styles.arrow0Animation : null}
+          <SwapOutlined
+            id={styles.arrowDownSwapSvg}
+            className={getArrowStyle(2) ? styles.arrow90Animation : null}
           />
         </div>
-        <img id={styles.storeModule} src={store} alt="store" />
+        <img
+          id={styles.storeModule}
+          className={scheduleRepair ? styles.shakeAnimation : null}
+          src={store}
+          alt="store"
+        />
       </div>
       <div className={styles.rightSection}>
         <SwapOutlined
@@ -128,7 +152,7 @@ const PredictionSystem = () => {
         />
       </div>
       <Modal
-        width={'75%'}
+        width={'60%'}
         styles={{ body: { marginTop: 30, maxHeight: '650px', overflow: 'auto' } }}
         open={isVcdpModalOpen}
         onCancel={() => setIsVcdpModalOpen(false)}
@@ -142,22 +166,50 @@ const PredictionSystem = () => {
             close
           </Button>
         ]}
-      />
+      >
+        <div>
+          <img className={styles.photoSquare} src={carsInLine} alt="carsInLine" />
+          <ArrowRightOutlined className={styles.carDataLakeSwap} />
+          <img
+            className={styles.photoSquare}
+            style={{ float: 'right' }}
+            src={dataLake}
+            alt="dataLake"
+          />
+        </div>
+      </Modal>
       <Modal
-        width={'75%'}
+        width={'50%'}
         styles={{ body: { marginTop: 30, maxHeight: '650px', overflow: 'auto' } }}
         open={isClientModalOpen}
         onCancel={() => closeClientModal()}
         title={'client'}
         footer={[
+          <Button
+            type={'primary'}
+            key="submit"
+            className={styles.closeBtn}
+            onClick={() => confirmClientModal()}
+          >
+            confirm
+          </Button>,
           <Button key="submit" className={styles.closeBtn} onClick={() => closeClientModal()}>
-            close
+            cancel
           </Button>
         ]}
       >
         <div>
           {vehicleUploadData?.engine_condition === 'unhealthy' ? (
-            <Table columns={vehicleDColforShown} dataSource={[vehicleUploadData]} />
+            <div>
+              <Alert message="coolant over-temperature" type="error" showIcon closable />
+              <div className={styles.AISuggestion}>AI suggestions: book time 4s store</div>
+              <DatePicker
+                minDate={dayjs('2024-07-04', dateFormat)}
+                maxDate={dayjs('2024-07-25', dateFormat)}
+                showTime={{ format: 'HH' }}
+              />
+              <div></div>
+            </div>
           ) : (
             <Skeleton active />
           )}
@@ -181,6 +233,22 @@ const PredictionSystem = () => {
       >
         <div>
           <Table columns={vehicleDataCol} dataSource={vehicleData} />
+        </div>
+      </Modal>
+      <Modal
+        width={'75%'}
+        styles={{ body: { marginTop: 30, maxHeight: '650px', overflow: 'auto' } }}
+        open={isMLModalOpen}
+        onCancel={() => setIsMLModalOpen(false)}
+        title={'Machine Learning Model'}
+        footer={[
+          <Button key="submit" className={styles.closeBtn} onClick={() => setIsMLModalOpen(false)}>
+            close
+          </Button>
+        ]}
+      >
+        <div>
+          <img className={styles.trainingResult} src={trainingResult} alt="trainingResult" />
         </div>
       </Modal>
     </div>
